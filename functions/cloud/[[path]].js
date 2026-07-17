303d70a815ddd20ff27713d1bef278d82e6806c0
ANGELHOWARD12 <48458847+ANGELHOWARD12@users.noreply.github.com>
2026-07-17 17:18:20 -0500

    Use supported password encryption

--- functions/cloud/[[path]].js
+++ functions/cloud/[[path]].js
@@ -356,7 +356,7 @@ function cookieValue(value, maxAge) {
 async function hashPassword(password, salt = randomToken(16)) {
   const key = await crypto.subtle.importKey("raw", new TextEncoder().encode(password), "PBKDF2", false, ["deriveBits"]);
   const bits = await crypto.subtle.deriveBits(
-    { name: "PBKDF2", hash: "SHA-256", salt: fromBase64Url(salt), iterations: 120000 },
+    { name: "PBKDF2", hash: "SHA-256", salt: fromBase64Url(salt), iterations: 100000 },
     key,
     256
   );
